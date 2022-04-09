cd522a72fcdcce807cc238965f5ae78752d10d8b
Fridah-kalee <fridahkalee@gmail.com>
2022-04-09 08:44:42 +0300

    declaring completeDate constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,10 +3,12 @@ export class Quote {
     quote: string;
     upvotes: number;
     downvotes: number;
+    completeDate: Date;
     constructor(id:number, quote:string, upvotes:number, downvotes:number, completeDate: Date){
     this.id=id;
     this.quote=quote;
     this.upvotes=upvotes;
     this.downvotes=downvotes;
+    // this.completeDate=Date;
     }
 }
