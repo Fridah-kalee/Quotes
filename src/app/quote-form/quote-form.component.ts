0040bfa267c86322e0fd3a16194dc471e2bc5a67
Fridah-kalee <fridahkalee@gmail.com>
2022-04-10 07:30:46 +0300

    fixing changes on EventEmitter

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { EventEmitter } from 'stream';
-import { Quote } from '../quote'
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
